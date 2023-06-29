613536c63e9fa83e68764486e079a9bc9f6a5ccd
seohee_Yun <cobbt258@naver.com>
2023-06-29 10:21:18 +0900

    TImeline

--- ScrollTrigger/js/scrollTrigger.js
+++ ScrollTrigger/js/scrollTrigger.js
@@ -1,16 +1,32 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".square", {
-  x: 700,
-  duration:8,
+// gsap.to(".box", {
+//   x: 500,
+//   duration: 2,
+// });
+// gsap.to(".box", {
+//   y: 200,
+//   duration: 3,
+//   delay: 2,
+// });
+// gsap.to(".box", {
+//   x: 0,
+//   duration: 2,
+//   delay: 5,
+// });
+
+// 위 내용을 아래처럼 정리 가능
+
+const tl = gsap.timeline({
   scrollTrigger: {
-    trigger: ".square2",
+    trigger: ".box",
+    markers: true,
     start: "top 80%",
     end: "top 30%",
-    scrub: 4,
-    toggleActions: "restart none none none",
-    pin: ".square",
-    pinSpacing: true,
-    markers: true,
-  },
+    scrub: 1
+  }
 });
+
+tl.to(".box", {x: 500, duration: 2,})
+  .to(".box", {y: 200, duration: 3,})
+  .to(".box", {x: 0, duration: 3,})
