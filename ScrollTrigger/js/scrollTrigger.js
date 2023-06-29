288b1443f382f83193a3eb5a74c9aa4b18ce457e
seohee_Yun <cobbt258@naver.com>
2023-06-29 10:00:31 +0900

    Pin

--- ScrollTrigger/js/scrollTrigger.js
+++ ScrollTrigger/js/scrollTrigger.js
@@ -4,11 +4,13 @@ gsap.to(".square", {
   x: 700,
   duration:8,
   scrollTrigger: {
-    trigger: ".square",
+    trigger: ".square2",
     start: "top 80%",
     end: "top 30%",
     scrub: 4,
     toggleActions: "restart none none none",
+    pin: ".square",
+    pinSpacing: true,
     markers: true,
   },
 });
