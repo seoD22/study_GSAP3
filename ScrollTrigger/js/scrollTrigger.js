6d9719763eeae8ad64679aa4d4e383c44acc085a
seohee_Yun <cobbt258@naver.com>
2023-06-29 10:51:53 +0900

    Standalone ScrolTrigger
    독립실행형 스크롤 트리거 만들기

--- ScrollTrigger/js/scrollTrigger.js
+++ ScrollTrigger/js/scrollTrigger.js
@@ -1,32 +1,8 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// gsap.to(".box", {
-//   x: 500,
-//   duration: 2,
-// });
-// gsap.to(".box", {
-//   y: 200,
-//   duration: 3,
-//   delay: 2,
-// });
-// gsap.to(".box", {
-//   x: 0,
-//   duration: 2,
-//   delay: 5,
-// });
-
-// 위 내용을 아래처럼 정리 가능
-
-const tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".box",
-    markers: true,
-    start: "top 80%",
-    end: "top 30%",
-    scrub: 1
-  }
+ScrollTrigger.create({
+  markers:true,
+  start: "top 6%",
+  trigger: ".panel",
+  toggleClass: {targets: "nav", className: "nav-active"}
 });
-
-tl.to(".box", {x: 500, duration: 2,})
-  .to(".box", {y: 200, duration: 3,})
-  .to(".box", {x: 0, duration: 3,})
