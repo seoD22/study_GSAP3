6ecb9a5d9eb2ad6c4a2cdbabfb87ab6d6a0d8c64
seohee_Yun <cobbt258@naver.com>
2023-07-13 10:32:58 +0900

    ㅇㅇ

--- ScrollTrigger/js/scrollTrigger.js
+++ ScrollTrigger/js/scrollTrigger.js
@@ -1,17 +1,1 @@
 gsap.registerPlugin(ScrollTrigger);
-
-const tl = gsap.timeline();
-
-tl.to(".box", {x: 500, duration: 5})
-  .to(".box", {y: 500, duration: 5})
-  .to(".box", {rotate: 90, repeat: 2, ease: "back"})
-
-tl.addLabel("label_1", 3) // tl 타임라인의 3초 지점에 'label'라는 라벨을 생성
-  .play("label_1") // lable_1부터 play 
-
-// ScrollTrigger.create({
-//   animation: tl,
-//   trigger: ".box",
-//   start: "top center",
-//   // scrub: 1,
-// })
